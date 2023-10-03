8686b2974e42652dc218928478a9b2cc12f51185
erykglowacki1 <B00153656@mytudublin.ie>
2023-10-03 21:21:42 +0100

    attempt to post webgl

---
+++ uni3challenge/Assets/Challenge 3/Scripts/OpenGit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenGithub()
+
+    {
+        Application.OpenURL("https://github.com/erykglowacki1");
+    }
+}
